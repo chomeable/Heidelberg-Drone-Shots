649b47e7bf985e90e3112feb8974d67f6983ffd4
MTZGER <tibor.gy.molnar@gmail.com>
2022-07-19 14:17:44 +0200

    finished list in admin page

--- pages/api/admin.js
+++ pages/api/admin.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 import { db, setup_tables } from '../../scripts/db'
 
 setup_tables()
@@ -7,12 +8,21 @@ export default function handler(req, res) {
     res.setHeader("Content-Type", "text/html")
     res.send(`
       <form action="/api/admin" method="POST">
-        <input type="password" name="password" />
+        <input type="password" name="password" autofocus />
       </form>
     `)
   } else if (req.method == 'POST' && req.body.password == 'testing') {
-    res.send('logged in')
-  }
+    db.all('SELECT name, email, typ, message FROM assignments', (err, rows) => {
+      if (err) return res.status(500).json({ message: err.message })
 
-    // if (req.params.)
+      res.setHeader("Content-Type", "text/html")
+
+      let ths = rows.length > 0 ? `<tr>${ Object.keys(rows[0]).map(e => `<th>${e}</th>`).join('\n') }</tr>` : ''
+      let tds = rows.map(e => `<tr>${ Object.values(e).map(i => `<td>${i}</td>`).join('\n') }</tr>`).join('\n')
+
+      res.send(fs.readFileSync('./scripts/admin.html').toString().replace('{{data}}', ths + tds))
+    
+    })
+
+  }
 }
