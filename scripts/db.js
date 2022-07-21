2561bd64d0eb5ca9ae901c2c623d3d53566ca7da
MTZGER <tibor.gy.molnar@gmail.com>
2022-07-21 18:51:54 +0200

    change db path

--- scripts/db.js
+++ scripts/db.js
@@ -1,6 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
+import path from 'path';
 
-const db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database(path.join(process.cwd(), 'db', 'database.db'), sqlite3.OPEN_READWRITE, (err) => {
   if (err)
     return console.error(err.message);
   console.log('Connected to the in-memory SQLite database.');
