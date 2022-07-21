17c8e4dd9359db9306ee3202b2860827d1de4aea
robin <80066142+robinnlmn@users.noreply.github.com>
2022-07-21 09:03:22 +0200

    database fix?

--- scripts/db.js
+++ scripts/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err)
     return console.error(err.message);
   console.log('Connected to the in-memory SQLite database.');
