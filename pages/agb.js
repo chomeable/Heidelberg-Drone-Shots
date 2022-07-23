1fecf58170b753201fab8d0bb742b1da1db81591
robin <80066142+robinnlmn@users.noreply.github.com>
2022-07-23 15:52:57 +0200

    AGB

---
+++ pages/agb.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from '../styles/Agb.module.css'
+
+export default function agb() {
+    return (
+        <div className={styles.agb}>
+            <h1>Die Allgemeine Geschäftsbedingungen</h1>
+
+            <h2>§1 Geltungsbereich</h2>
+            <p>Die Allgemeine Geschäftsbedingungen (nachstehend „AGB“) gelten für alle Verträge zwischen Droneshots Heidelberger und dem Nutzer (nachstehend „Nutzer“).</p>
+            <p>Die AGB können jederzeit von Droneshots Heidelberger angepasst werden.</p>
+            <p>Die AGB gelten für alle Verträge zwischen Droneshots Heidelberger und dem Nutzer.</p>
+
+
+        </div>
+    )
+}
