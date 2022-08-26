1d78bfac3c8d5381546db11f3d473c5a23e2d54e
robin <80066142+robinnlmn@users.noreply.github.com>
2022-08-26 09:04:33 +0200

    equipments section

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,7 @@ export default function Home() {
       }
     }
 
+    renderSlider()
     recalculate()
   }, [])
   // useEffect(() => {
@@ -77,6 +78,47 @@ export default function Home() {
     pauschal: 'privat',
     aufwand: 1,
     schnitt: true,
+  }
+
+  const drones = [
+    {
+      img: "https://geprc.com/wp-content/uploads/2022/06/16-2.jpg",
+      text: "Eine Propellergeschützte Drohne optimal für Indoor flüge und ermöglicht es nahe an Personen zu fliegen."
+    },
+    {
+      img: "https://ae01.alicdn.com/kf/H4a6bc7c42aa34aa88db0a84438139074i/IFlight-Nazgul5-V2-240mm-5-zoll-4S-6S-FPV-Drone-BNF-mit-SucceX-E-F4-45A.jpg_Q90.jpg_.webp",
+      text: "Eine schnelle und wendige Drohne gut zu nutzen für Verfolgungsshots oder Freestyle."
+    }
+  ]
+
+  let slider = 0;
+
+  function updateSlider(value) {
+    // console.log(drones.length)
+    // console.log(drones[1])
+    // console.log(slider + value)
+    if (slider + value < 0) {
+      slider = drones.length - 1
+      console.log(slider)
+      renderSlider()
+    }
+    else if (slider + value > drones.length - 1) {
+      slider = 0
+      console.log(slider)
+      renderSlider()
+    }
+    else {
+      slider = slider + value
+      console.log(slider)
+      renderSlider()
+    }
+  }
+
+  function renderSlider() {
+    const text = document.getElementById("sliderText")
+    const img = document.createElement("sliderImg")
+    text.innerHTML = drones[slider].text
+    sliderImg.src = drones[slider].img
   }
 
   const recalculate = e => {
@@ -136,7 +178,7 @@ export default function Home() {
             <img src='/drone-icon.ico' height='70px' /> 
           </div> */}
           {/* <img src='/drone-icon.ico' /> */}
-          <embed src="/DH-Logo.svg" height="175" width="175"/>
+          <embed src="/DH-Logo.svg" height="175" width="175" />
           <br />
 
           <h1 className={styles.title}>
@@ -180,9 +222,9 @@ export default function Home() {
             </div>
           </div>
 
-          <div>       
-            <h2 style={{color: "white"}}>Infos zu diesen Aufnahmen</h2>
-            <ul style={{color: "white"}}>
+          <div>
+            <h2 style={{ color: "white" }}>Infos zu diesen Aufnahmen</h2>
+            <ul style={{ color: "white" }}>
               <li>Bis zu 150 km/h</li>
               <li>Gut für freiluft Umgebungen</li>
               <li>Aufgenommen mit einer GoPro Hero 9</li>
@@ -205,7 +247,7 @@ export default function Home() {
             </svg>
           </div>
           <form action="https://formsubmit.co/0da830855b437249ecd6df8913af57a0" method="POST" className={styles.form}>
-          {/* <form action="/api/form" method="POST" className={styles.form}> */}
+            {/* <form action="/api/form" method="POST" className={styles.form}> */}
             <h1>KONTAKT</h1>
             <div className={styles.formRow}>
               <label>NAME *</label>
@@ -241,77 +283,77 @@ export default function Home() {
           <br />
           <br />
           <div className={styles.prices}>
-          <table className={styles.table}>
-            <thead>
-              <tr className={styles.tablehead}>
-                <th>BETREFF</th>
-                <th>PREIS</th>
-                <th>ERKLÄRUNG</th>
-              </tr>
-            </thead>
+            <table className={styles.table}>
+              <thead>
+                <tr className={styles.tablehead}>
+                  <th>BETREFF</th>
+                  <th>PREIS</th>
+                  <th>ERKLÄRUNG</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              <tr>
-                <td className={styles.tdtitle}>Pauschalbetrag</td>
-                <td>50€ bei Privatpersonen <br /> 75€ bei Unternehmen</td>
-                <td className={styles.tdinfo}>Eine Gebühr, die bei <u>jedem</u> Auftrag verbindlich ist.</td>
-              </tr>
-              <tr>
-                <td className={styles.tdtitle}>Aufwand</td>
-                <td>12,50€ - 120,00€</td>
-                <td className={styles.tdinfo}>Abhängig vom <u>Schwierigkeitsgrad</u> und <u>Zeitaufwand</u> der Aufnahme.</td>
-              </tr>
-              <tr>
-                <td className={styles.tdtitle}>Gerätekosten</td>
-                <td>30€</td>
-                <td className={styles.tdinfo}>Die Kosten für die <u>Wartung</u> des Gerätes in diesem Fall der Drohne.</td>
-              </tr>
-              <tr>
-                <td className={styles.tdtitle}>Videoschnitt</td>
-                <td>25€</td>
-                <td className={styles.tdinfo}>Professioneller Schnitt des Videos nach Wünschen des Kunden.</td>
-              </tr>
-              <tr>
-                <td className={styles.tdtitle}>Stabilisation + Farbkorrektur</td>
-                <td><s>20€</s>    <strong>Kostenlos</strong></td>
-                <td className={styles.tdinfo}>Eine Stabilisierung und Farbkorrektur der Aufnahme ist <u>inklusive</u>.</td>
-              </tr>
-            </tbody>
-          </table>
+              <tbody>
+                <tr>
+                  <td className={styles.tdtitle}>Pauschalbetrag</td>
+                  <td>50€ bei Privatpersonen <br /> 75€ bei Unternehmen</td>
+                  <td className={styles.tdinfo}>Eine Gebühr, die bei <u>jedem</u> Auftrag verbindlich ist.</td>
+                </tr>
+                <tr>
+                  <td className={styles.tdtitle}>Aufwand</td>
+                  <td>12,50€ - 120,00€</td>
+                  <td className={styles.tdinfo}>Abhängig vom <u>Schwierigkeitsgrad</u> und <u>Zeitaufwand</u> der Aufnahme.</td>
+                </tr>
+                <tr>
+                  <td className={styles.tdtitle}>Gerätekosten</td>
+                  <td>30€</td>
+                  <td className={styles.tdinfo}>Die Kosten für die <u>Wartung</u> des Gerätes in diesem Fall der Drohne.</td>
+                </tr>
+                <tr>
+                  <td className={styles.tdtitle}>Videoschnitt</td>
+                  <td>25€</td>
+                  <td className={styles.tdinfo}>Professioneller Schnitt des Videos nach Wünschen des Kunden.</td>
+                </tr>
+                <tr>
+                  <td className={styles.tdtitle}>Stabilisation + Farbkorrektur</td>
+                  <td><s>20€</s>    <strong>Kostenlos</strong></td>
+                  <td className={styles.tdinfo}>Eine Stabilisierung und Farbkorrektur der Aufnahme ist <u>inklusive</u>.</td>
+                </tr>
+              </tbody>
+            </table>
 
 
-          <div>
-            <div className={styles.calculator}>
-              <h3 style={{ fontSize: '25px', padding: '0' }}>Rechner</h3>
-              <div className={styles.radiobtns}>
-                <div>
-                  <input type="radio" name="pauschal" defaultChecked value="privat" id="privatperson" onInput={recalculate} />
-                  <label htmlFor="privatperson">Privatpersonen</label>
+            <div>
+              <div className={styles.calculator}>
+                <h3 style={{ fontSize: '25px', padding: '0' }}>Rechner</h3>
+                <div className={styles.radiobtns}>
+                  <div>
+                    <input type="radio" name="pauschal" defaultChecked value="privat" id="privatperson" onInput={recalculate} />
+                    <label htmlFor="privatperson">Privatpersonen</label>
+                  </div>
+                  {/* &lt;&gt; */}
+                  <div>
+                    <input type="radio" name="pauschal" value="unternehmen" id="unternehmer" onInput={recalculate} />
+                    <label htmlFor="unternehmer">Unternehmen</label>
+                  </div>
                 </div>
-                {/* &lt;&gt; */}
-                <div>
-                  <input type="radio" name="pauschal" value="unternehmen" id="unternehmer" onInput={recalculate} />
-                  <label htmlFor="unternehmer">Unternehmen</label>
+                <div className={styles.calculatorRow}>
+                  <label htmlFor="aufwand">Aufwand: </label>
+                  <input type="number" name="aufwand" max={10} min={1} id="aufwand" onInput={recalculate} defaultValue={1} />
                 </div>
-              </div>
-              <div className={styles.calculatorRow}>
-                <label htmlFor="aufwand">Aufwand: </label>
-                <input type="number" name="aufwand" max={10} min={1} id="aufwand" onInput={recalculate} defaultValue={1} />
-              </div>
-              <div className={styles.calculatorRow} name="schnitt">
-                <label htmlFor="aufwand">Schnitt </label>
-                <input type="checkbox" name="schnitt" id="schnitt" onInput={recalculate} defaultChecked={true} />
-              </div>
+                <div className={styles.calculatorRow} name="schnitt">
+                  <label htmlFor="aufwand">Schnitt </label>
+                  <input type="checkbox" name="schnitt" id="schnitt" onInput={recalculate} defaultChecked={true} />
+                </div>
 
-              <div className={styles.calculatorRow}>
-                <div className={styles.inputs}>
-                  <h2 id="calculatorResult">0€</h2>
+                <div className={styles.calculatorRow}>
+                  <div className={styles.inputs}>
+                    <h2 id="calculatorResult">0€</h2>
+                  </div>
+                  <p className={styles.warn}>* Dies sind nur <strong>geschätzte</strong> Summen zur Orientierung.</p>
                 </div>
-                <p className={styles.warn}>* Dies sind nur <strong>geschätzte</strong> Summen zur Orientierung.</p>
               </div>
             </div>
           </div>
-        </div>
         </div>
 
         <div className={styles.sec4}>
@@ -319,6 +361,43 @@ export default function Home() {
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
               <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className={styles.shapefill}></path>
             </svg>
+          </div>
+        </div>
+
+        <div className={styles.sec5}>
+          <h1 className={styles.title}>EQUIPMENT</h1>
+
+          <br />
+
+          <div className={styles.equipments}>
+            <div className={styles.equipment}>
+              <h2 className={styles.subtitle}>DROHNEN</h2>
+              <br />
+              <div className={styles.slider}>
+                <button className={styles.sliderButton} onClick={() => updateSlider(-1)}>
+                  &#x25C0;
+                </button>
+                {
+
+                }
+                <img src="https://geprc.com/wp-content/uploads/2022/06/16-2.jpg" id="sliderImg" />
+                <button className={styles.sliderButton} onClick={() => updateSlider(1)}>
+                  &#x25B6;
+                </button>
+              </div>
+              <p id="sliderText">
+              </p>
+            </div>
+
+            <div className={styles.equipment}>
+              <h2 className={styles.subtitle}>KAMERA</h2>
+              <br />
+              <img src="https://res-5.cloudinary.com/grover/image/upload/e_trim/c_limit,f_auto,fl_png8.lossy,h_1280,q_auto,w_1280/v1600703377/w3qbobl36ahnmowjxjlb.png" />
+              <p>
+                Die Kamera die zur Aufnahme genutzt wird ist eine GoPro Hero 9 Black. <br />
+                Sie wird gemeinsam mit einem ND filter für optimale farben und belichtung genutzt.
+              </p>
+            </div>
           </div>
         </div>
       </main >
@@ -384,8 +463,10 @@ export default function Home() {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span> */}
         </Link>
-        
+
       </footer>
+
+      <br />
     </div >
   )
 }
